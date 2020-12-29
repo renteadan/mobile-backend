c3647fb98f9df0432b7a7adb637bbaf59142118b
renteadan <62014192+renteadan@users.noreply.github.com>
2020-12-29 16:25:09 +0200

    add payload limit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { json } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<any> {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
+	app.use(json({
+		limit: '50mb',
+	}));
 	await app.listen(3000);
 }
 bootstrap();
